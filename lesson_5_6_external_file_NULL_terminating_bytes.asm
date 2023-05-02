a333e6eb9c46791041a8551218c34551ccdbd69e
twk-mn <fred@tawakemono.com>
2023-05-02 11:22:12 +0900

    Changed the name of the functions file in lesson 5 and 6.

--- lesson_5_6_external_file_NULL_terminating_bytes.asm
+++ lesson_5_6_external_file_NULL_terminating_bytes.asm
@@ -1,4 +1,4 @@
-%include        'functions.asm'
+%include        'lesson_5_6_functions.asm'
 
 SECTION .data
 msg1    db  'Hello, brave new world', 0Ah, 0h           ; 0h = null terminating byte
@@ -9,9 +9,9 @@ global _start
 
 _start:
     mov     eax, msg1   ; Adding the address of msg1 to EAX
-    call    sprint      ; Call the subroutine 'sprint' in functions.asm 
+    call    sprint      ; Call the subroutine 'sprint' in lesson_5_6_functions.asm 
 
-    mov     eax, msg2   ; Call the subroutine 'sprint' in functions.asm
+    mov     eax, msg2   ; Call the subroutine 'sprint' in lesson_5_6_functions.asm
     call    sprint
 
-    call quit           ; Calls quit  from functions.asm
+    call quit           ; Calls quit  from lesson_5_6_functions.asm
