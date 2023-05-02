c522a8942047803ecee10e5af57c7ef420f69b76
twk-mn <fred@tawakemono.com>
2023-05-02 17:11:21 +0900

    Added functions.asm

---
+++ Lesson_9-user_input/functions.asm
@@ -0,0 +1,61 @@
+; int slen(str msg)
+; Str len calc func
+
+slen:
+    push    ebx
+    mov     ebx, eax
+
+nextchar:
+    cmp     byte [eax], 0
+    jz      finished
+    inc     eax
+    jmp     nextchar
+
+finished:
+    sub     eax, ebx
+    pop     ebx
+    ret
+
+; void sprint (str msg)
+; Str printing func
+
+sprint:
+    push    edx
+    push    ecx
+    push    ebx
+    push    eax
+    call    slen
+
+    mov     edx, eax
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+
+
+    pop     ebx
+    pop     ecx
+    pop     edx
+    ret
+
+; void stringLF(str msg)
+; Str printing with LF func
+
+sprintLF:
+    call    eax
+
+    push    eax
+    mov     eax, 0Ah
+    push    eax
+    mov     eax, esp
+    call    sprint
+    pop     eax
+    pop     eax
+    ret
+
+; void exit()
+; Exit prog andn restore resources
+quit:
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+    ret
