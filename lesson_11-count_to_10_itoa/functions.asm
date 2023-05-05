057556536d5c1b3dc7eb217696f0590fa7a7a99d
twk-mn <fred@tawakemono.com>
2023-05-05 13:35:19 +0900

    Finished function.asm

--- lesson_11-count_to_10_itoa/functions.asm
+++ lesson_11-count_to_10_itoa/functions.asm
@@ -45,3 +45,66 @@ iprintLF:
     pop     eax         ; remove LF from stack
     pop     eax         ; restore the original value for EAX
     ret
+
+; int slen(Str msg)
+; Str len calc func
+
+slen:
+    push    ebx
+    mov     ebx, eax
+
+nextchar:
+    cmp     byte [eax], 0
+    jz      finished
+    inc     eax
+    jmp     nextchar
+
+finished:
+    sub     eax, ebx
+    pop     ebx
+    ret
+
+; void sprint(str msg)
+;Str printing func
+
+sprint:
+    push    edx
+    push    ecx
+    push    ebx
+    push    eax
+    call    slen
+
+    mov     edx, eax
+    pop     eax
+
+    mov     ecx, eax
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+
+    pop     ebx
+    pop     ecx
+    pop     edx
+    ret
+
+; void sprintLF
+; Str print w LF func
+sprintLF:
+    call    sprint
+
+    push    eax
+    mov     eax, 0Ah
+    push    eax
+    mov     eax, esp
+    call    sprint
+    pop     eax
+    pop     eax
+    ret
+
+; void exit()
+; exit the prog and reset resources
+quit:
+    mov     ebx, 0
+    move    eax, 1
+    int     80h
+    ret
