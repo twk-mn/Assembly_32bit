d1e34232b3f5f57c24ea74d17512807877ff3529
twk-mn <fred@tawakemono.com>
2023-05-09 17:58:05 +0900

    Added content to readme.md for lesson 15

--- Lesson_15-calc_division/lesson_15.asm
+++ Lesson_15-calc_division/lesson_15.asm
@@ -8,13 +8,13 @@ global  _start
 
 _start:
 
-    mov     eax, 0x90
-    mov     ebx, 0x9
-    div     ebx
-    call    iprint
-    mov     eax, msg1
-    call    sprint
-    mov     eax, edx
-    call    iprintLF
+    mov     eax, 0x90   ; Set EAX to 90
+    mov     ebx, 0x9    ; Set EBX to 90
+    div     ebx         ; Divide EAX with EBX
+    call    iprint      ; call int print func for the qoutient (stored in EAX, EDX gets the remainder)
+    mov     eax, msg1   ; move msg1 into EAX
+    call    sprint      ; call string print func
+    mov     eax, edx    ; move the remainder into EAX
+    call    iprintLF    ; Call int print with LF func
 
-    call quit
+    call quit           ; exit
